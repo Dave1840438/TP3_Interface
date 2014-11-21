2259de13bb860a35dd0fcd458a3acd80f0e60bbe
Dave1840438 <david.sylvestre@sympatico.ca>
2014-11-21 12:01:51 -0500

    Choix des heures fini*
    *Par contre il faut changer le form principal parce que les propriétés
    ont changés

---
+++ Compact Agenda/CustomSelectControl.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Compact_Agenda
+{
+    public partial class CustomSelectControl : UserControl
+    {
+        public int MinimumValue { get; set; }
+        public int MaximumValue { get; set; }
+
+        private int _leap;
+
+        public int Leap { get {return _leap;} set { if (value == 0) _leap = 1; else _leap = value;  } }
+        
+        public int StartValue { get; set; }
+
+        int _currentValue;
+
+        public int Value { get { return Int32.Parse(textBox1.Text); } }
+
+        public CustomSelectControl()
+        {
+            _leap = 1;
+            InitializeComponent();
+            _currentValue = (StartValue / _leap) * _leap;
+            UpdateTextBoxValue();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _currentValue += Leap;
+
+            if (_currentValue > MaximumValue)
+                _currentValue = MinimumValue;
+
+            UpdateTextBoxValue();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _currentValue -= Leap;
+
+            if (_currentValue < MinimumValue)
+                _currentValue = (MaximumValue / _leap) * _leap;
+
+            UpdateTextBoxValue();
+        }
+
+        private void UpdateTextBoxValue()
+        {
+            textBox1.Text = _currentValue.ToString();
+        }
+    }
+}
